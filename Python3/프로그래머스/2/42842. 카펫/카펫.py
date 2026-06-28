d41869a72f1e64fbd240dfd6bf0253595d6185a9
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-06-28 20:59:26 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,11 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    for height in range(1, total + 1):
+        if total % height == 0:
+            width = total // height
+            
+        if height > width:
+            continue
+        
+        if (width - 2) * (height - 2) == yellow:
+            return [width, height]
